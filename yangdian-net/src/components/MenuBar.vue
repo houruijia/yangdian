acc3d23fa08691e45769b5e7c936ea323382ac15
houruijia <1042771903@qq.com>
2018-10-16 17:55:59 +0800

    22

--- yangdian-net/src/components/MenuBar.vue
+++ yangdian-net/src/components/MenuBar.vue
@@ -3,22 +3,26 @@
 		<ul>
 			<li>
 				<router-link to="/">
-					<span>首页</span>
+					<span class="iconfont">&#xe627;</span>
+					<span class="text">首页</span>
 				</router-link>
 			</li>
 			<li>
 				<router-link to="Classify">
-					<span>分类</span>
+					<span class="iconfont">&#xe617;</span>
+					<span class="text">分类</span>
 				</router-link>
 			</li>
 			<li>
-				<router-link to="/shopCar">
-					<span>购物车</span>
+				<router-link to="/ShopCar">
+					<span class="iconfont">&#xe606;</span>
+					<span class="text">购物车</span>
 				</router-link>
 			</li>
 			<li>
 				<router-link to="/User">
-					<span>我</span>
+					<span class="iconfont">&#xe633;</span>
+					<span class="text">我</span>
 				</router-link>
 			</li>
 		</ul>
@@ -29,6 +33,42 @@
 		name:"MenuBar"
 	}
 </script>
+
 <style scoped>
+	.MenuBar{
+		width:100%;
+		height: 100px;
+		position: fixed;
+		bottom: 0;
+	}
+	.MenuBar ul{
+		width: inherit;
+		height : inherit;
+		display: flex;
+	}
+	.MenuBar ul li{
+		width: 25%;
+		height: inherit;
+	}
+	.MenuBar ul li a{
+		display: flex;
+		height: 100px;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+	.MenuBar ul li a span.iconfont{
+		fontSize: 38px;
+	}
+	.MenuBar ul li a span.text{
+		fontSize: 24px;
+	}
+	.MenuBar ul li a span{
+		display:block;
+		margin: 7px 0px;
+	}
+	.MenuBar ul li a.router-link-exact-active{
+		color: #fb9b00;
+	}
 	
 </style>
--- yangdian-net/src/main.js
+++ yangdian-net/src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
-import global_menuBar from '.plugins/global_menuBar.js'
-
+import App from './App.vue'
+import router from './router'
+import global_menuBar from './plugins/global_menuBar.js'
+import 'lib-flexible/flexible.js'
+import './style/common.css'
+import './style/iconfont.css'
 Vue.config.productionTip=false
 
 Vue.use(global_menuBar)
